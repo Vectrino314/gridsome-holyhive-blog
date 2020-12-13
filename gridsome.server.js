12603607e9a04e29bbb3a3968f3d0b2fbf14e9a4
Victor Solis <victor.solis@tylertech.com>
2020-12-13 13:43:37 -0700

    build error fix

--- gridsome.server.js
+++ gridsome.server.js
@@ -16,7 +16,7 @@ module.exports = function (api) {
   api.onCreateNode(options => {
     if (options.internal.typeName === 'Blog') {
       options.featured = (options.featured) ? options.featured : false;
-      options.tags = (typeof options.tags === 'string') ? options.tags.split(',').map(string => string.trim()) : options.tags;
+      options.tags = (typeof options.tags === 'string') ? options.tags.split('-').map(string => string.trim()) : options.tags;
       options.author = (typeof options.author === 'string') ? options.author.split(',').map(string => string.trim()) : options.author;
       return {
         ...options
@@ -55,9 +55,7 @@ module.exports = function (api) {
       //without the map, you will get an 500 error
       //because the graphql filter requires an array
       //not an object
-
-      // the tags are now an array so no need for the map function
-      var tags = (node.tags, function (tag) {
+      var tags = _.map(node.tags, function (tag) {
         return tag.title;
       });
 
--- posts/testing.md
+++ posts/testing.md
@@ -2,8 +2,6 @@
 title: testing
 categories: []
 author: Laney D. Fowler
-thumbnail: "/uploads/lola.jpg"
-datetime: '2020-12-13T00:00:00Z'
 post_bg_color: "#594040"
 post_fg_color: "#B14242"
 image: "/uploads/lola.jpg"
